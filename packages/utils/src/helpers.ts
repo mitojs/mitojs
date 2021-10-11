5623c235d8aa89fca6f155d8bf368c4e4b6b540a
chenjinhuo <chenjinhuo@bytedance.com>
2021-10-11 22:30:03 +0800

    chore(helpers): add annotation on replaceOld

--- packages/utils/src/helpers.ts
+++ packages/utils/src/helpers.ts
@@ -37,13 +37,13 @@ export function on(
 }
 
 /**
- *
  * 重写对象上面的某个属性
- * ../param source 需要被重写的对象
- * ../param name 需要被重写对象的key
- * ../param replacement 以原有的函数作为参数，执行并重写原有函数
- * ../param isForced 是否强制重写（可能原先没有该属性）
- * ../returns void
+ *
+ * @export
+ * @param {IAnyObject} source 需要被重写的对象
+ * @param {string} name 需要被重写对象的key
+ * @param {(...args: any[]) => any} replacement 以原有的函数作为参数，执行并重写原有函数
+ * @param {boolean} [isForced=false] 是否强制重写（可能原先没有该属性）
  */
 export function replaceOld(source: IAnyObject, name: string, replacement: (...args: any[]) => any, isForced = false): void {
   if (source === undefined) return
